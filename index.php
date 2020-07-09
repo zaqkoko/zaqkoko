42ecd2173cd77fe880848c158c0aa67a6d4f833c
zaqkoko <zaqkoko@naver.com>
2020-07-10 00:15:54 +0900

    1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB</title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li>HTML</li>
+    </ol>
+    <a href="create.php">create</a>
+    <h2>Welcome</h2>  
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+</body>
+</html>
