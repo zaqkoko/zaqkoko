141be065c5716d0c46dc4a7526d95bb46c5129a2
zaqkoko <zaqkoko@naver.com>
2020-07-11 23:06:19 +0900

    1

--- insert.php
+++ insert.php
@@ -1,5 +1,8 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "04540121", "opertutorials");
+// mysqli_connect = php에서 mysql을 연결해주는 함수 (반대는 mysqli_close)
+$conn = mysqli_connect('localhost', 'root', '04540121', 'opertutorials');
+
+// INSERT = 테이블에 레코드를 삽입하기 위해 사용하는 구문.
 $sql = "
   INSERT INTO topic (
     title,
@@ -10,9 +13,15 @@ $sql = "
      'MySQL is ....',
      NOW()
   )";
+
+// mysqli_query = mysqli_connect를 통해 연결된 객체를 이용하며 mysql 쿼리를 실행시키는 함수
 $result = mysqli_query($conn, $sql);
+
 if($result === false){
     echo mysqli_error($conn);
+}else{
+  mysqli_query($conn, $sql);
+  echo '성공했습니다 <a href="index.php">돌아가기<a/>';
 }
 
 ?>
--- process_create.php
+++ process_create.php
@@ -1,5 +1,8 @@
 <?php
+// mysqli_connect = php에서 mysql을 연결해주는 함수 (반대는 mysqli_close)
 $conn = mysqli_connect('localhost', 'root', '04540121', 'opentutorials');
+
+// INSERT = 테이블에 레코드를 삽입하기 위해 사용하는 구문.
 $sql = "
   INSERT INTO topic
     (title, description, created)
@@ -9,5 +12,7 @@ $sql = "
         NOW()
     )
 ";
-mysqli_query($conn, $sql);
+// mysqli_query = mysqli_connect를 통해 연결된 객체를 이용하며 mysql 쿼리를 실행시키는 함수
+mysqli_query($conn, $sql); 
 echo $sql;
+?>
