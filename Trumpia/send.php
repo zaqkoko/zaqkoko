7913385e3e255f6d8e5e83d8066851627d19b86d
zaqkoko <zaqkoko@naver.com>
2020-07-15 14:23:06 +0900

    1

--- Trumpia/send.php
+++ Trumpia/send.php
@@ -1,33 +1,30 @@
 <!DOCTYPE html>
 <html lang="ko">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>ToySend</title>
-  </head>
-  <body>
-    <br />
 
-    <form action="send1.php" method="POST">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>ToySend</title>
+</head>
 
-      <input type="datetime-local", type="submit" id="send_time" name="send_time">
-      <div style="text-align: center;"></div>
-      <script>
-        document.getElementById('send_time').value= new Date().toISOString().slice(0, 16);
-      </script>
+<body>
+  <br />
 
-        <textarea
-          name="sms_text"
-          cols="40"
-          rows="30"
-          placeholder="메세지를 입력하세요"
-        ></textarea>
-        <div style="font-size: 12px; padding-top: 3px; padding-bottom: 10px;">
-          <span class="bold">글자 수 제한</span>&nbsp;0/150
-        </div>
-        <input type="submit" />
+  <form action="send1.php" method="POST">
 
-    </form>
-  </body>
+    <input type="datetime" , type="submit" id="send_time" name="send_time">
+    <div style="text-align: center;"></div>
+    <script>
+      document.getElementById('send_time').value = new Date().toISOString().slice(0, 16);
+    </script>
+
+    <textarea name="sms_text" cols="40" rows="30" placeholder="메세지를 입력하세요"></textarea>
+    <div style="font-size: 12px; padding-top: 3px; padding-bottom: 10px;">
+      <span class="bold">글자 수 제한</span>&nbsp;0/150
+    </div>
+    <input type="submit" />
+
+  </form>
+</body>
 
 </html>
